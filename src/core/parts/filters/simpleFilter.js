2de85d55fba042b6d95e87742da45d65e5b5631c
Oleg Smith <olegsmith@gmail.com>
2013-08-27 00:01:06 +0400

    fix filter-input

--- src/core/parts/filters/simpleFilter.js
+++ src/core/parts/filters/simpleFilter.js
@@ -1,197 +1,121 @@
 angular.module('core')
 
-	.directive('filterInput', ['$timeout',
-		function($timeout) {
-			return {
-				/* This one is important: */
-				scope: {},
-				compile: function(element, attrs, transclude) {
+.directive('filterInput', ['$timeout',
+	function($timeout) {
+		return {
+			/* This one is important: */
+			scope: {},
+			compile: function(element, attrs, transclude) {
 
-					var filterNgModel = attrs.filterNgModel,
-						path = attrs.path;
+				var filterNgModel = attrs.filterNgModel;
 
-					/* The trick is here: */
-					// if (attrs.ngModel) {
-					//     attrs.$set('ngModel', '$parent.' + attrs.ngModel+'.val', false);
-					// }
-					// ------Not working
-					//attrs.$set('ngModel', '$parent.' + filterNgModel + '.val');
-					//attrs.$set('uiSelect2', 'lookupOptions');
-					//
-					// element.attr("ng-model", '$parent.' + filterNgModel + '.val');
-					// element.attr("ui-select2", 'lookupOptions');
-					// element[0].setAttribute("ng-model", '$parent.' + filterNgModel + '.val');
-					// element[0].setAttribute("ui-select2", 'lookupOptions');
+				/* The trick is here: */
+				// if (attrs.ngModel) {
+				//     attrs.$set('ngModel', '$parent.' + attrs.ngModel+'.val', false);
+				// }
+				// ------Not working
+				//attrs.$set('ngModel', '$parent.' + filterNgModel + '.val');
+				//attrs.$set('uiSelect2', 'lookupOptions');
+				//
+				// element.attr("ng-model", '$parent.' + filterNgModel + '.val');
+				// element.attr("ui-select2", 'lookupOptions');
+				// element[0].setAttribute("ng-model", '$parent.' + filterNgModel + '.val');
+				// element[0].setAttribute("ui-select2", 'lookupOptions');
 
-					element.replaceWith('<div><input ng-model="$parent.' + filterNgModel + '.val"/></div>');
+				element.replaceWith('<div><input ng-model="$parent.' + filterNgModel + '.val"/></div>');
 
-					return function($scope, element, attrs) {
+				return function($scope, element, attrs) {
 
+					var opts = $scope.$eval(attrs.filterInput);
 
-						function prop2JSON(props, val) {
-							var cursor = val,
-								collect;
-							for (var i = props.length - 1; i >= 0; i--) {
-								collect = {};
-								collect[props[i]] = cursor;
-								cursor = collect;
-							}
-							return collect;
+					//console.log(opts);
+
+					function prop2JSON(props, val) {
+						var cursor = val,
+							collect;
+						for (var i = props.length - 1; i >= 0; i--) {
+							collect = {};
+							collect[props[i]] = cursor;
+							cursor = collect;
+						}
+						return collect;
+					}
+
+					var props = filterNgModel.split('.');
+					//props.push('state');
+
+					var state = prop2JSON(props, opts);
+
+					element.bind('keyup', function() {
+						$scope.$apply(function() {
+							$.extend(true, $scope.$parent, state);
+						});
+					});
+				};
+			}
+		};
+	}
+])
+
+.directive('filterLookup', ['$timeout',
+	function($timeout) {
+		return {
+			/* This one is important: */
+			scope: {},
+			compile: function(element, attrs, transclude) {
+
+				var filterNgModel = attrs.filterNgModel;
+
+				element.replaceWith('<div><input ng-model="$parent.' + filterNgModel + '.val" ui-select2="lookupOptions" style="width:200px;"/></div>');
+
+				return function($scope, element, attrs) {
+
+					var opts = $scope.$eval(attrs.filterLookup);
+
+					function prop2JSON(props, val) {
+						var cursor = val,
+							collect;
+						for (var i = props.length - 1; i >= 0; i--) {
+							collect = {};
+							collect[props[i]] = cursor;
+							cursor = collect;
+						}
+						return collect;
+					}
+
+					var props = filterNgModel.split('.');
+
+					var state = prop2JSON(props, opts);
+
+					element.bind('change', function() {
+						$scope.$apply(function() {
+							$.extend(true, $scope.$parent, state);
+						});
+					});
+				};
+			},
+
+			controller: ["$scope", "$element", "$attrs", "$http", "$timeout",
+				function($scope, $element, $attrs, $http, $timeout) {
+
+					$scope.lookupOptions = {
+						multiple: true,
+						query: function(query) {
+							//console.log("in query");
+
+							$timeout(function() {
+								$http.post($attrs.action).then(function(response) {
+									query.callback({
+										results: response.data.rows
+									});
+								});
+							});
 						}
 
-						var props = filterNgModel.split('.');
-						props.push('state');
-
-						var state = prop2JSON(props, {
-							path: path
-						});
-
-						element.bind('keyup', function() {
-							$scope.$apply(function() {
-								$.extend(true, $scope.$parent, state);
-							});
-						});
 					};
 				}
-			};
-		}
-	])
+			]
 
-	.directive('filterLookup', ['$timeout',
-		function($timeout) {
-			return {
-				/* This one is important: */
-				scope: {},
-				compile: function(element, attrs, transclude) {
-
-					var filterNgModel = attrs.filterNgModel,
-						path = attrs.path;
-
-					element.replaceWith('<div><input ng-model="$parent.' + filterNgModel + '.val" ui-select2="lookupOptions" style="width:200px;"/></div>');
-
-					return function($scope, element, attrs) {
-
-						function prop2JSON(props, val) {
-							var cursor = val,
-								collect;
-							for (var i = props.length - 1; i >= 0; i--) {
-								collect = {};
-								collect[props[i]] = cursor;
-								cursor = collect;
-							}
-							return collect;
-						}
-
-						var props = filterNgModel.split('.');
-						props.push('state');
-
-						var state = prop2JSON(props, {
-							path: path
-						});
-
-						element.bind('change', function() {
-							$scope.$apply(function() {
-								$.extend(true, $scope.$parent, state);
-							});
-						});
-					};
-				},
-
-				controller: ["$scope",
-					function($scope) {
-						var categories = [{
-							id: 1,
-							text: 'cat'
-						}, {
-							id: 2,
-							text: 'dog'
-						}, {
-							id: 3,
-							text: 'pet'
-						}, {
-							id: 4,
-							text: 'rat'
-						}, {
-							id: 5,
-							text: 'fat'
-						}, {
-							id: 6,
-							text: 'zet'
-						}];
-
-						$scope.lookupOptions = {
-							multiple: true,
-							query: function(query) {
-								$timeout(function() {
-									var data = {
-										results: categories
-									};
-									query.callback(data);
-								}, 400);
-							}
-						};
-
-					}
-				]
-
-			};
-		}
-	]);
-
-
-
-
-/*!!! version with template
-
- .directive('filterLookup',  function($compile, $timeout) {
- return {
- restrict: 'A',
- template: '<div><input type="text" ui-select2="options" ng-model="bar" style="style"/></div>',
- scope: {
- bar: '=ngModel',
- style: '@style'
- },
- require: 'ngModel',
- replace: true,
- controller: function($scope, $element, $attrs) {
-
-
-
- var categories = [{
- id: 1,
- text: 'cat'
- }, {
- id: 2,
- text: 'dog'
- }, {
- id: 3,
- text: 'pet'
- }, {
- id: 4,
- text: 'rat'
- }, {
- id: 5,
- text: 'fat'
- }, {
- id: 6,
- text: 'zet'
- }];
-
-
-
- $scope.options = {
- multiple: true,
- query: function(query) {
- $timeout(function() {
- var data = {
- results: categories
- };
- query.callback(data);
- }, 400);
- }
- };
- }
-
- };
- })
- */
+		};
+	}
+]);
