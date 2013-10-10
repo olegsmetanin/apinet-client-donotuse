c519bb785ea30c1ca009ea9ae869caf68baf28b3
Artem Naumov <naumov.artem@gmail.com>
2013-10-10 16:18:15 +0400

    Bug refs #1399: fixed validation issues

--- src/core/directives/integer.js
+++ src/core/directives/integer.js
@@ -1,21 +1,28 @@
 angular.module('core')
-    .directive('integer', function() {
-        var INTEGER_REGEXP = /^\-?\d*$/;
-
-        return {
-            require: 'ngModel',
-            link: function(scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function(viewValue) {
-                    if (INTEGER_REGEXP.test(viewValue)) {
-                    // it is valid
-                        ctrl.$setValidity('integer', true);
-                        return viewValue;
-                    } else {
-                        // it is invalid, return undefined (no model update)
-                        ctrl.$setValidity('integer', false);
-                        return undefined;
-                    }
-                });
-            }
-        };
-    });
+.directive('agoInteger', function() {
+	var INTEGER_REGEXP = /^\-?\d*$/;
+	return {
+		require: 'ngModel',
+		link: function(scope, elm, attrs, ctrl) {
+			var min = attrs.agoMin ? parseInt(attrs.agoMin) : null;
+			var max = attrs.agoMax ? parseInt(attrs.agoMax) : null;
+			ctrl.$parsers.unshift(function(viewValue) {
+				if (INTEGER_REGEXP.test(viewValue)) {
+					// it is valid
+					var ival = parseInt(viewValue);
+					if ((min !== null && ival < min) || (max !== null && ival > max)) {
+						// it is invalid, return undefined (no model update)
+						ctrl.$setValidity('integer', false);
+						return undefined;    
+					}
+					ctrl.$setValidity('integer', true);
+					return viewValue;
+				} else {
+					// it is invalid, return undefined (no model update)
+					ctrl.$setValidity('integer', false);
+					return undefined;
+				}
+			});
+		}
+	};
+});
