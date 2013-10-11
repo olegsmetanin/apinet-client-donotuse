41a6343512f940798d678ae2c8f6f5cfcf3d0733
Румянцев Алексей <igexxx@gmail.com>
2013-10-11 19:38:27 +0900

    Сортировка

--- src/core/directives/grids.js
+++ src/core/directives/grids.js
@@ -10,6 +10,15 @@ angular.module('core')
 			templateUrl: sysConfig.src('core/directives/filters/sortableHeading.tpl.html'),
 
 			controller: ['$scope', function($scope) {
+				angular.extend($scope, {
+					ascButtonText: function() {
+						return $scope.sorter && $scope.sorter.direction === 'desc' ? $scope.sorter.priority : '▲';
+					},
+					descButtonText: function() {
+						return $scope.sorter && $scope.sorter.direction === 'asc' ? $scope.sorter.priority : '▼';
+					}
+				});
+
 				$scope.$watch('sorter.direction', function(value) {
 					if(!value && $scope.sorter) {
 						$scope.sorter.priority = null;
