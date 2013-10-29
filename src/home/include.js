de7e692b4dd1e41f5dc38b733bd2560a39211077
Румянцев Алексей <igexxx@gmail.com>
2013-10-29 15:29:13 +0900

    Создание проектов из списка

--- src/home/include.js
+++ src/home/include.js
@@ -5,7 +5,6 @@ sysConfig.modules['home'] = {
 	js: [
 		'src/home/_module.js',
 		'src/home/projects/listview/projectsList.js',
-		'src/home/projects/create/createProject.js',
 		'src/home/projects/statuses/projectStatuses.js',
 		'src/home/projects/tags/projectTags.js'
 	]
