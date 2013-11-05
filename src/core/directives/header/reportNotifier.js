1c5cb0c04ccb2b29d2fb816d7d2640e846096c9b
Румянцев Алексей <igexxx@gmail.com>
2013-11-05 18:37:53 +0900

    requirejs

---
+++ src/core/directives/header/reportNotifier.js
@@ -0,0 +1,69 @@
+define(['angular', '../../moduleDef', 'text!./eventNotifier.tpl.html'], function (angular, module, tpl) {
+	module.directive('reportNotifier', ['security', 'sysConfig', 'reportService', '$timeout',
+		function(security, sysConfig, reportService, $timeout) {
+			var directive = {
+				templateUrl: sysConfig.src('core/parts/header/reportNotifier/reportNotifier.tpl.html'),
+				restrict: 'EA',
+				replace: true,
+				scope: true,
+				link: function($scope, $element, $attrs, $controller) {
+
+					$scope.showGenerating = false;
+
+					$scope.showDone = false;
+
+					$scope.badge = '';
+
+					$scope.updating = false;
+
+
+					function update() {
+						var reports = reportService.getUnreadUserReports(),
+							generating = [],
+							done = [];
+						for (var i = 0; i < reports.length; i++) {
+							if (reports[i].Status === 'done') {
+								done.push(angular.copy(reports[i]));
+							} else {
+								generating.push(angular.copy(reports[i]));
+							}
+						}
+
+						$timeout(function() {
+
+							$scope.generating = generating;
+							$scope.showGenerating = generating.length !== 0;
+							$scope.done = done;
+							$scope.showDone = done.length !== 0;
+							$scope.badge = (generating.length === 0 ? (done.length === 0 ? '' : done.length) : generating.length + '/' + done.length);
+
+						});
+
+						$scope.updating = false;
+					}
+
+					$scope.$on('events:unreadReportsChanged', function() {
+						update();
+					});
+
+					$scope.updateUnreadUserReports = function() {
+
+						$scope.updating = true;
+						reportService.updateUnreadUserReports();
+
+					};
+
+					$scope.cancelReportGeneration = function(index) {
+						reportService.cancelReportGeneration($scope.reports.gen[index].name);
+					};
+
+
+					$timeout(function() {
+						update();
+					});
+
+				}
+			};
+			return directive;
+		}
+	]);
