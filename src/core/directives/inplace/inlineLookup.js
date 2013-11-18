27d701368cb6770b706bd0e60bff1c66873b638f
Румянцев Алексей <igexxx@gmail.com>
2013-11-18 13:35:22 +0900

    fix лукапов в задачах

--- src/core/directives/inplace/inlineLookup.js
+++ src/core/directives/inplace/inlineLookup.js
@@ -5,16 +5,14 @@ define(['angular', '../../moduleDef'], function (angular, module) {
 			replace: true,
 			transclude: true,
 			template: function(elm, attr) {
-				var viewTmpl = attr.hasOwnProperty('multiple')
-					? '<div style="display: inline-block" ng-transclude></div>'
-					: '{{ ' + attr.model + '.text }}';
+				var viewTmpl = attr.hasOwnProperty('multiple') ?
+					'<div style="display: inline-block" ng-transclude></div>'	: '{{ ' + attr.model + '.text }}';
 
 				var editTmpl = '<input type="text" ng-model="emodel.value" ng-readonly="waiting"' +
 					' lookup="' + attr.inputLookup + '"' +
 					' class="form-control ' + (attr.inputClass ? attr.inputClass + '"' : '"') +
-					(attr.hasOwnProperty('required')
-						? (attr.hasOwnProperty('multiple') ? ' required-multiple="true"' : ' ng-required="true"')
-						: '') +
+					(attr.hasOwnProperty('required') ? (attr.hasOwnProperty('multiple') ?
+						' required-multiple="true"' : ' ng-required="true"') : '') +
 					(attr.hasOwnProperty('multiple') ? ' multiple="multiple"' : '') +
 					' lookup-options="{openOnEnter: false' +
 						(attr.inputLookupOptions ?  + ', ' + attr.inputLookupOptions : '') + '}"></input>';
@@ -36,45 +34,35 @@ define(['angular', '../../moduleDef'], function (angular, module) {
 				//select2 not initialized if call without timeout
 				$timeout(function() {
 					//inplaceEdit use this for focus() when enter in edit mode
-					scope.elInput = attr.hasOwnProperty('multiple')
-						? $('input.select2-input', elm)
-						: $('.select2-focusser', elm);
+					scope.elInput = attr.hasOwnProperty('multiple')	?
+						$('input.select2-input', elm) : $('.select2-focusser', elm);
+
+					scope.onFocus = function() {
+						$(this).select2('open');
+					};
 
 					var $input = $('input', elm);
 					//select2 has self specific blur
 					$input.off('blur', scope.onBlur);
 
 					$input.on('select2-blur', scope.onBlur);
+					$input.on('select2-focus', scope.onFocus);
+
 					$input.on('select2-opening', function(e) {
 						$input.off('select2-blur', scope.onBlur);
+						$input.off('select2-focus', scope.onFocus);
 					});
 					$input.on('select2-close', function(e) {
-						//if attach without timeout, blur handled right after close
 						$timeout(function() {
 							$input.on('select2-blur', scope.onBlur);
-							//console.log('lookup focused: %s', $input.is(':focus'));
+							$input.on('select2-focus', scope.onFocus);
+
 							if (!attr.hasOwnProperty('multiple')) {
 								scope.onBlur();
-							} else {
-								// $timeout(function() {
-								// 	console.log('close select2Change: %s', scope.select2Change);
-								// 	if (!angular.isDefined(scope.select2Change) || scope.select2Change === false) {
-								// 		//scope.onBlur();
-								// 	}
-								// }, 10);
-								//console.log('lookup focused: %s', $input.is(':focus'));
 							}
 						});
 						return false;
 					});
-					$input.on('select2-focus', function(e) {
-						$(this).select2('open');
-					});
-					// $input.on('change', function(e) {
-					// 	scope.select2Change = true;
-					// 	console.log('change select2Change: %s', scope.select2Change);
-					// });
-					//enter and esc handling does not work, because of killEvent in select2 code
 				}, 50);
 			}
 		};
