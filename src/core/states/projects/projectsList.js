44ce964ff17158c2c91210b42e807915b27215f0
Румянцев Алексей <igexxx@gmail.com>
2014-01-21 18:47:57 +0900

    Отображение тегов

--- src/core/states/projects/projectsList.js
+++ src/core/states/projects/projectsList.js
@@ -20,58 +20,7 @@
 				template: tpl
 			});
 		}])
-		.controller('projectsListCtrl', ['$scope', '$window', 'apinetService', function($scope, $window, apinetService) {
-			angular.extend($scope, {
-				createItemForm: { },
-				editModel: { },
-				deleteModel: { },
-				validation: { },
-
-				handleException: function(error) {
-					$scope.resetValidation();
-					$scope.validation.generalErrors = [error];
-				},
-
-				handleValidationErrors: function(validation) {
-					$scope.resetValidation();
-					angular.extend($scope.validation, validation);
-				},
-
-				createItem: function() {
-					$scope.resetValidation();
-					delete $scope.editModel.id;
-
-					if ($scope.editModel.Type && $scope.editModel.Type.id) {
-						$scope.editModel.Type = $scope.editModel.Type.id;
-					}
-
-					apinetService.action({
-						method: 'core/projects/createProject',
-						model: $scope.editModel
-					}).then(function(result) {
-						if(typeof result.success === 'undefined' || result.success) {
-							$scope.dropChanges();
-							$scope.models.unshift(result);
-						}
-						else {
-							$scope.handleValidationErrors(result);
-						}
-					}, $scope.handleException);
-				},
-
-				dropChanges: function() {
-					$scope.resetValidation();
-					$scope.editModel = { };
-					$scope.createItemForm.$setPristine();
-					$scope.createItemForm.$setValidity('integer', true);
-				},
-
-				createEnabled: function() {
-					return $scope.createItemForm.$valid && $scope.createItemForm.$dirty &&
-						(typeof $scope.validation.success === 'undefined' || $scope.validation.success);
-				}
-			});
-
+		.controller('projectsListCtrl', ['$scope', '$window', 'apinetService', function($scope) {
 			$scope.$on('resetFilter', function() {
 				$scope.filter.simple = { Participation: 'All' };
 				$scope.requestParams.mode = 'All';
